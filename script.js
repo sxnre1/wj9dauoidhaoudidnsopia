e86209ae0a9c0bd82d4118608d376da4da4c8aff
unknown <senleodaaauwa@gmail.com>
2025-10-27 23:04:37 +0900

    .26

--- script.js
+++ script.js
@@ -2,33 +2,6 @@ const faders = document.querySelectorAll('.fade-in');
 const hamburger = document.querySelector('.hamburger');
 const btn = document.querySelector(".hamburger-btn");
 const nav = document.querySelector("header nav");
-const bgm = document.getElementById("bgm");
-
-window.addEventListener("load", () => {
-  const playPromise = bgm.play();
-
-    if (playPromise !== undefined) {
-      playPromise
-        .then(() => {
-          bgm.volume = 0;
-            const maxVolume = 0.3;
-            const fadeDuration = 5000;
-            const intervalTime = 100;
-            const volumeStep = maxVolume / (fadeDuration / intervalTime);
-
-            const fadeIn = setInterval(() => {
-              if (bgm.volume < maxVolume) {
-                bgm.volume = Math.min(bgm.volume + volumeStep, maxVolume);
-              } else {
-                clearInterval(fadeIn);
-              }
-            }, intervalTime);
-          })
-          .catch((err) => {
-        console.log("자동재생이 차단되었습니다:", err);
-      });
-    }
-});
 
 btn.addEventListener("click", () => {
   nav.classList.toggle("active");
