5dbf8441dbaaff87d95b96ce48f9d03a4a9b0d53
unknown <senleodaaauwa@gmail.com>
2025-10-27 18:51:19 +0900

    script.js patch

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const faders = document.querySelectorAll('.fade-in');
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('header nav');
+const btn = document.querySelector(".hamburger-btn");
+const xIcon = document.querySelectorAll(".x-icon");
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+
+  hamburger.forEach(span => {
+    span.style.opacity = nav.classList.contains("active") ? 0 : 1;
+  });
+  xIcon.forEach(span => {
+    span.style.opacity = nav.classList.contains("active") ? 1 : 0;
+  });
+});
+
 
 const appearOptions = {
   threshold: 0.2,
